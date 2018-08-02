eea08df813bdbcdaea9ec7d71ba55522fd7e1bb0
joeheald85 <joeheald85@users.noreply.github.com>
2018-08-02 13:42:32 -0400

    add accept-encoding header for ruby 2.x compatibility (#1)
    * add accept-endcoding header for ruby 2.x compatibility
    
    * use 'none' instead of identity
    
    * actually allow args[:header] for future flexibility

--- lib/rets/http.rb
+++ lib/rets/http.rb
@@ -9,6 +9,7 @@ module RETS
     # Creates a new HTTP instance which will automatically handle authenting to the RETS server.
     def initialize(args)
       @headers = {"User-Agent" => "Ruby RETS/v#{RETS::VERSION}"}
+      @headers.merge!(args.delete(:headers)) if args[:headers]
       @request_count = 0
       @config = {:http => {}}.merge(args)
       @rets_data, @cookie_list = {}, {}
